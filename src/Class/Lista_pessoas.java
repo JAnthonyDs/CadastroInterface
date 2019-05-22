81b47fd04492cf377e85b13e3b8d32b8bb406f64
Anthony Dantas <anthonydantas15@gmail.com>
2019-05-22 19:36:13 -0300

    Commit

---
+++ src/Class/Lista_pessoas.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class;
+import java.util.ArrayList;
+/**
+ *
+ * @author Meus documentos
+ */
+public class Lista_pessoas {
+    static ArrayList<Pessoa> lista = new ArrayList<Pessoa>();
+    
+    public ArrayList retorno_lista(){
+        return lista;
+    }
+    
+}
---
+++ src/Class/Pessoa.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Meus documentos
+ */
+public class Pessoa {
+    private String nome;
+    private String cpf;
+    private String email;
+    public static int num_pessoas;
+    
+    public Pessoa(){
+        num_pessoas++;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
+ 
+}
